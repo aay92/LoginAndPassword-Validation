2e7a1bc78113f0fb566647f542b02d8229d76873
Aleksey <aay92@yandex.ru>
2022-06-26 12:29:24 +0400

    Initial Commit

---
+++ LoginAndPassword+Validation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoginAndPassword+Validation
+//
+//  Created by Aleksey Alyonin on 26.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
